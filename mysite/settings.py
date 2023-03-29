13187d75c6ac9dc22be75bfe961033bc96eb782d
mahdiramazani <mahdiramazani128@gmail.com>
2023-03-29 10:29:51 +0430

    add new branch

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,8 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-gh%$=*d9o_sndsvqvgjc-5(g*zbgb%$@661#)##bl_3e%ck(%!'
